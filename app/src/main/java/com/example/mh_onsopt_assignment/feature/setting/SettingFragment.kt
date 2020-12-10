61ae843c076088974c3dce62f10e3097ae87c99f
Haeeul <etoilune00@gmail.com>
2020-12-11 07:27:27 +0900

    [feat] #4 GridLayout 추가

--- app/src/main/java/com/example/mh_onsopt_assignment/feature/setting/SettingFragment.kt
+++ app/src/main/java/com/example/mh_onsopt_assignment/feature/setting/SettingFragment.kt
@@ -5,14 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.mh_onsopt_assignment.R
+import com.example.mh_onsopt_assignment.databinding.FragmentSettingBinding
+import com.example.mh_onsopt_assignment.feature.portfolio.PortfolioAdapter
+import com.example.mh_onsopt_assignment.vo.DummyPortfolioList
 
 
 class SettingFragment : Fragment() {
+    lateinit var binding: FragmentSettingBinding
+    lateinit var portfolioAdapter: SettingAdapter
+    private var portfolioList = DummyPortfolioList()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_setting, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        initRvList()
+    }
+
+    private fun initRvList(){
+        portfolioAdapter = SettingAdapter(requireContext())
+
+        binding.rvPortfolio2.adapter = portfolioAdapter
+
+        portfolioAdapter.data = portfolioList.getPortfolioList()
+
+        portfolioAdapter.notifyDataSetChanged()
     }
 }
